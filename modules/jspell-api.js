554589084279d7c2961547bda1678c6f2637f31b
Brady <brady.davenport@pm.me>
2022-05-31 16:18:53 -0600

    spell check api

--- modules/jspell-api.js
+++ modules/jspell-api.js
@@ -1,20 +1,51 @@
-// STARTER CODE FROM JSPELL DOCUMENTATION
+'use strict';
 
-// const axios = require("axios");
+const axios = require('axios');
 
-// const options = {
-//   method: 'POST',
-//   url: 'https://jspell-checker.p.rapidapi.com/check',
-//   headers: {
-//     'content-type': 'application/json',
-//     'X-RapidAPI-Host': 'jspell-checker.p.rapidapi.com',
-//     'X-RapidAPI-Key': ''
-//   },
-//   data: '{"language":"enUS","fieldvalues":"thiss is intresting","config":{"forceUpperCase":false,"ignoreIrregularCaps":false,"ignoreFirstCaps":true,"ignoreNumbers":true,"ignoreUpper":false,"ignoreDouble":false,"ignoreWordsWithNumbers":true}}'
-// };
+const options = {
+  method: 'POST',
+  url: 'https://jspell-checker.p.rapidapi.com/check',
+  headers: {
+    'content-type': 'application/json',
+    'X-RapidAPI-Host': 'jspell-checker.p.rapidapi.com',
+    'X-RapidAPI-Key': `${process.env.JSPELL_API_KEY}`
+  },
+  data: `{
+    "language": "enUS",
+    "fieldvalues": "this is intresting",
+    "config": {
+      "forceUpperCase": false,
+      "ignoreIrregularCaps": false,
+      "ignoreFirstCaps": true,
+      "ignoreNumbers": true,
+      "ignoreUpper": false,
+      "ignoreDouble": false,
+      "ignoreWordsWithNumbers": true
+    }
+  }`
+};
 
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
+let spellCheck = () => {
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+    return response.data;
+  }).catch(function (error) {
+    console.error(error);
+  });
+};
+
+
+// async function spellCheck(req, res, next) {
+//   try {
+//     let errorsResponse = await axios.request(options);
+//     if (errorsResponse.spellingErrorCount !== 0) {
+//       res.status(200).send(errorsResponse.elements[0].errors);
+//     } else {
+//       res.status(200).send([]);
+//     }
+//   } catch (e) {
+//     next(e);
+//   }
+// }
+
+module.exports = spellCheck;
--- modules/updateTerm.js
+++ modules/updateTerm.js
@@ -1,12 +1,16 @@
 'use strict';
 
 const Term = require('../models/termSchema.js');
+// const spellCheck = require('./jspell-api.js');
 
-async function updateTerm (req, res, next) {
+async function updateTerm(req, res, next) {
   try {
-    const { term_name, definition, user_email, category, documentation_url } = req.body;
-    const updatedTerm = await Term.findByIdAndUpdate(req.params.id, { term_name, definition, user_email, category, documentation_url }, {new: true, overwrite: true});
+    const { term_name, definition, user_email } = req.body;
+    // let spellChecked = spellCheck();
+    // if (spellChecked.spellingErrorCount !== 0) {
+    const updatedTerm = await Term.findByIdAndUpdate(req.params.id, { term_name, definition, user_email }, { new: true, overwrite: true });
     res.status(200).send(updatedTerm);
+    // }
   } catch (e) {
     next(e);
   }
